3a988f89ca51931ccebd6e6b97654f58b922470c
lwj <2290870457@qq.com>
2026-05-31 17:05:37 +0800

    lwj测试1

--- mis-gateway/src/main/java/edu/scau/mis/gateway/MisGatewayApplication.java
+++ mis-gateway/src/main/java/edu/scau/mis/gateway/MisGatewayApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class MisGatewayApplication {
+    //测试1
     public static void main(String[] args) {
         SpringApplication.run(MisGatewayApplication.class, args);
     }
